b5890f942d9e380efc838c1d4bcadc163d9cdf2d
Michael McGreal <mcgrealife@gmail.com>
2023-01-18 11:51:41 -0500

    testing how vercel handles base url in different node_env

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log('api request received')
+  res.status(200).json({ name: 'this is a response from the API' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,43 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [data, setData] = useState('')
+
+  console.log(process.env)
+
+  const baseUrl = () => {
+    switch (process.env.NODE_ENV) {
+      case 'development':
+        return `http://localhost:${process.env.PORT ?? 3000} `
+      case 'preview':
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      default:
+        return '' // if production, then empty string
+    }
+  }
+  const getPage = async () => {
+    const res = await fetch(`${baseUrl()}/api/getPage`)
+    const data = await res.json()
+    console.log('data', data)
+  }
+
+
+
+  const handleFetch = async () => {
+    console.log(`starting fetch with url ${baseUrl()}/api/hello`)
+    const res = await fetch(`${baseUrl()}/api/hello`, {
+      method: 'GET',
+      application: 'application/json',
+    })
+    console.log('res', res)
+    const data = await res.json()
+    console.log('data', data)
+    setData(data.name)
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +47,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          process.env.NODE_ENV:
+          <code className={styles.code}>{process.env.NODE_ENV}</code>
+        </p>
+        <p className={styles.description}>
+          process.env.VERCEL_URL:
+          <code className={styles.code}>{process.env.VERCEL_URL}</code>
+        </p>
+        <p className={styles.description}>
+          process.env.NEXT_PUBLIC_VERCEL_URL:
+          <code className={styles.code}>{process.env.NEXT_PUBLIC_VERCEL_URL}</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div>Fetch to url: {baseUrl()}/api/hello </div>
+        <button onClick={handleFetch}>{`fetch`}</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <div>Fetch response: {data}</div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
